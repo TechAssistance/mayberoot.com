1f11e28d2b1ee9a3611d106e8adbf0326e5fa14d
TechAssistance <justsomen337@gmail.com>
2024-07-05 11:13:12 -0700

    Nav bar goes brrrrrr

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-	import type { Content } from "@prismicio/client";
+	import { type Content, isFilled } from '@prismicio/client';
+	import NavBarLink from './NavBarLink.svelte';
+	import Button from './Button.svelte';
 
+	import IconMenu from '~icons/ic/baseline-menu';
+	import IconClose from '~icons/ic/baseline-close';
 
-export let settings: Content.SettingsDocument;
+	export let settings: Content.SettingsDocument;
 
-let open = false
+	let open = false;
 
-function onLinkClick(){
-}
+	function onLinkClick() {
+		open = false;
+	}
 </script>
 
 <header class="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4 relative">
@@ -20,6 +25,39 @@ function onLinkClick(){
                     OPEN
                 </button>
             </div>
-        </div>
+            <ul
+				class={`fixed inset-0 z-50 flex flex-col items-end gap-4 bg-slate-50 pr-4 pt-14 transition-transform duration-300 ease-in-out md:hidden ${open ? 'translate-x-0' : 'translate-x-[100%]'}`}
+			>
+				<li>
+					<button
+						aria-expanded={open}
+						aria-label="Close Menu"
+						class="fixed right-4 top-3 block p-2 text-2xl text-slate-800 md:hidden"
+						on:click={() => (open = false)}
+					>
+						<IconClose />
+					</button>
+				</li>
+				{#each settings.data.nav_item as { label, link }}
+					<li class="first:mt-8">
+						<NavBarLink field={link} {label} {onLinkClick} type="mobile" />
+					</li>
+				{/each}
+				{#if isFilled.link(settings.data.cta_link)}
+					<Button linkField={settings.data.cta_link} label={settings.data.cta_label} />
+				{/if}
+			</ul>
+
+			<!-- Desktop Nav -->
+			<ul class="relative z-50 hidden flex-row items-center gap-1 bg-transparent py-0 md:flex">
+				{#each settings.data.nav_item as { label, link }}
+					<li>
+						<NavBarLink field={link} {label} {onLinkClick} type="desktop" />
+					</li>
+				{/each}
+				{#if isFilled.link(settings.data.cta_link)}
+					<Button linkField={settings.data.cta_link} label={settings.data.cta_label} class="ml-3" />
+				{/if}
+			</ul>
     </nav>
 </header>
