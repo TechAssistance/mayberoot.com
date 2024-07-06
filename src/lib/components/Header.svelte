2b076cb3d0927dcc5efdaaa088e5751a305428f3
TechAssistance <justsomen337@gmail.com>
2024-07-06 12:24:03 -0700

    finally got it to the background !!!

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -16,16 +16,27 @@
 </script>
 
 <header class="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4 relative">
-    <nav>
-        <div class="flex flex-col justify-between rounded-b-lg bg-slate-50 px-4 py-2 md:m4 md:flex-row md:items-center md:rounded-xl">
-            <div class="flex items-center justify-between">
-                <a href="/" aria-label="Homepage"
-                class="text-xl font-extrabold tracking-tighter text-slate-900">{settings.data.name}</a>
-                <button aria-expanded={open} aria-label="Open Menu" class="block p-2 text-2xl text-state-800 md:hidden" on:click={() => (open = true)}>
-                    OPEN
-                </button>
-            </div>
-            <ul
+	<nav>
+		<div
+			class="flex flex-col justify-between rounded-b-lg bg-slate-50 px-4 py-2 md:m4 md:flex-row md:items-center md:rounded-xl"
+		>
+			<div class="flex items-center justify-between">
+				<a
+					href="/"
+					aria-label="Homepage"
+					class="text-xl font-extrabold tracking-tighter text-slate-900">{settings.data.name}</a
+				>
+				<button
+					aria-expanded={open}
+					aria-label="Open Menu"
+					class="block p-2 text-2xl text-slate-800 md:hidden"
+					on:click={() => (open = true)}
+				>
+					<IconMenu />
+				</button>
+			</div>
+			<!-- Mobile Nav -->
+			<ul
 				class={`fixed inset-0 z-50 flex flex-col items-end gap-4 bg-slate-50 pr-4 pt-14 transition-transform duration-300 ease-in-out md:hidden ${open ? 'translate-x-0' : 'translate-x-[100%]'}`}
 			>
 				<li>
@@ -59,5 +70,6 @@
 					<Button linkField={settings.data.cta_link} label={settings.data.cta_label} class="ml-3" />
 				{/if}
 			</ul>
-    </nav>
+		</div>
+	</nav>
 </header>
--- src/lib/slices/Hero/index.svelte
+++ src/lib/slices/Hero/index.svelte
@@ -64,19 +64,17 @@
 
 <svelte:window bind:scrollY={scroll} />
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} class="px-4 md:px-6">
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} class="">
 	<div>
 		<h3>{scroll}</h3>
 	</div>
-	<div>
-		<img class="background-image" src="1Stars.svg">
-	</div>
-    <div class="mx-auto w-full max-w-7xl">
+    <div class="mx-auto w-full">
+	<div class="bg-fixed bg-cover mx-auto w-full h-200" style="background-image: url(../../../../static/1Stars.svg);"> 
         <div class="grid min-h-[65vh] grid-cols-1 items-center md:grid-cols-2">
 			<div class="relative z-10 row-span-1 -my-10 aspect-[1/1.3] overflow-hidden md:col-span-1 md:col-start-2 md:mt-0">
             <Scene />
         </div>
-
+		
         <div class="col-start-1 md:row-start-1">
             <h1 class="mb-2 md:mb-8 text-[clamp(3rem,20vmin,8rem)] font-extrabold leading-none tracking-tighter text-nowrap"
             aria-label={slice.primary.welcome + ' ' + slice.primary.name}
@@ -99,6 +97,7 @@
             </span>
             </div>
         </div>
+	</div>
     </div>
 	<div class="box"></div>
 	<div class="box"></div>
